fa6b16150183a3a37769e8d731ead5b64ac5fee4
Crameriver <44029881+Crameriver@users.noreply.github.com>
2018-11-13 16:38:04 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('menu');
+var opener = document.getElementById('open-menu');
+opener.addEventListener('click', function() {
+  menu.classList.toggle('is-open');
+});
+
