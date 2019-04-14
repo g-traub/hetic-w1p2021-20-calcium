eb75998290bda0403b28b0b6cfd69830e1834a4d
gtrb <guillaume.trb@hotmail.com>
2019-04-14 11:51:03 +0200

    optimizing images formats and sizes

---
+++ src/js/main.js
@@ -0,0 +1,59 @@
+var menu = document.getElementById('menu');
+var opener = document.getElementById('open-menu');
+opener.addEventListener('click', function() {
+	menu.classList.toggle('is-open');
+	kappa.classList.toggle('transparent');
+	kapa.classList.toggle('transparent');
+});
+
+//closes the burger menu when a list item is clicked
+var list = document.getElementsByClassName('list');
+for (let index = 0; index < list.length; index++) {
+	list[index].addEventListener('click', function(){
+		menu.classList.remove('is-open');
+		kappa.classList.toggle('transparent');
+		kapa.classList.toggle('transparent');
+	})
+}
+
+//to avoid resizing to mobile size and having the burger menu open
+function myFunction(x) {
+	if (x.matches) { // If media query matches
+		menu.classList.remove('is-open');
+		kappa.classList.remove('transparent');
+		kapa.classList.add('transparent');
+	}
+}
+
+var x = window.matchMedia("(max-width: 960px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes 
+	
+var isScrolling;
+var nav = document.querySelector('nav');
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+  nav.classList.add('cake');
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+
+    nav.classList.remove('cake');
+	}, 400);
+
+}, false);
+
+const produits = document.getElementById('produits');
+if (produits){
+	var waypoint1 = new Waypoint({
+		element: produits,
+		handler: function() {
+			document.querySelector('.soba').classList.add('reached');
+			document.querySelector('.ramen').classList.add('reached');
+			document.querySelector('.noodles').classList.add('reached');
+		},
+		offset: 400
+	});
+}
